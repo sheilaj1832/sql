f0eb34682e0b8b1f9d59a6e965df6f25d475420f
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-09-02 22:29:50 -0400

    Updates

--- projects/Textbooks_db.sql
+++ projects/Textbooks_db.sql
@@ -1,26 +1,26 @@
 -- create and select the database
-DROP DATABASE IF EXISTS sample;
-CREATE DATABASE sample;
-USE sample;
+DROP DATABASE IF EXISTS textbooks;
+CREATE DATABASE textbooks;
+USE textbooks;
 
 -- create the Product table
 CREATE TABLE Product (
-  ID			INT            PRIMARY KEY  AUTO_INCREMENT,
+  ProductID	    INT            PRIMARY KEY  AUTO_INCREMENT,
   Textbook      VARCHAR(30)    NOT NULL,
   Price	        DECIMAL(10,2)  NOT NULL
 );
 
 -- create the Buyer Information table
 CREATE TABLE BuyerInformation (
-  ID			INT            PRIMARY KEY  AUTO_INCREMENT,
+  BuyerID		INT            PRIMARY KEY  AUTO_INCREMENT,
   Textbook      VARCHAR(30)    NOT NULL,
   Buyer	        VARCHAR(50)    NOT NULL
 );
 
 CREATE TABLE Invoice (
-  ID			INT              PRIMARY KEY  AUTO_INCREMENT,
+  InvoiceID		INT              PRIMARY KEY  AUTO_INCREMENT,
   Textbook      VARCHAR(30)      NOT NULL,
-  OrderNumber	INT              AUTO_INCREMENT NOT NULL
+  OrderNumber	INT              NOT NULL
 );
 
 -- insert some rows into the Product table
@@ -32,37 +32,29 @@ INSERT INTO Product VALUES
 INSERT INTO BuyerInformation VALUES
 (1, 'Thermodynamics', 'Rachel Baumann'),
 (2, 'Thermodynamics', 'John Falconer'),
-(3, 'Calculus 3', 'Neil Hendren');
+(3, 'Calculus 3', 'Neil Hendren'),
+(4, 'Fluid Dynamics', 'Tim Brown');
 
 INSERT INTO Invoice VALUES
 (1, 'Fluid Dynamics', 123),
 (2, 'Thermodynamics', 456),
-(3, 'Fluid Dynamics', 789);
+(3, 'Fluid Dynamics', 789),
+(4, 'Thermodynamics', 801);
 
 
 
-select pdt.ID, pdt.textbook, price, bi.id, bi.textbook, buyer
+select ProductID, pdt.textbook, price, BuyerID, bi.textbook, buyer
   from
    Product pdt
       inner join
     BuyerInformation bi
 	on pdt.textbook = bi.textbook;
     -- aligns the columns from both columns.
-    
-select bi.ID, bi.Textbook, Buyer, Inv.ID, inv.Textbook, OrderNumber
+
+select BuyerID, bi.textbook, buyer, InvoiceID, inv.Textbook, OrderNumber
   from
    BuyerInformation bi
       inner join
-    invoice inv 
-   on bi.Textbook = inv.Textbook; 
-   
-  -- 3 joins  
- select OrderNumber, CustomerName, OrderDate, Code, Description, ListPrice, Total
-  from
-   lineitem li
-      inner join
-    invoice inv 
-      on li.InvoiceID = inv.ID
-	  inner join
-	Product Pdt
-   on li.ProductID = pdt.ID;
+    Invoice inv
+	on bi.textbook = inv.textbook;
+    -- aligns the columns from both columns.
