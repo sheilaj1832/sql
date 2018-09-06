5af3c5a95e5697e5410511d510a7b735c31f3927
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-09-06 09:31:14 -0400

    Update to user information

--- Capstone/prs_create_db.sql
+++ Capstone/prs_create_db.sql
@@ -120,3 +120,6 @@ INSERT INTO product (ID, VendorID, PartNumber, Name, Price , Unit , PhotoPath) V
 INSERT INTO product (ID, VendorID, PartNumber, Name, Price , Unit , PhotoPath) VALUES (12,5,'228148','Acer Aspire ATC-780A-UR12 Desktop Computer',399.99,'','/images/AcerAspireDesktop.jpg');
 INSERT INTO product (ID, VendorID, PartNumber, Name, Price , Unit , PhotoPath) VALUES (13,5,'279364','Lenovo IdeaCentre All-In-One Desktop',349.99,'','/images/LenovoIdeaCenter.jpg');
 
+-- create a user and grant privledges
+CREATE USER prs_user@localhost IDENTIFIED BY 'sesame';
+GRANT SELECT, INSERT, DELETE, UPDATE ON prs.* TO prs_user@localhost;
